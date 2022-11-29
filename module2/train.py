223e82fba29114d58be25d09e47c8f49c928dc08
Laplas00 <20helllucifer02@gmail.com>
2022-11-29 14:41:31 +0200

    pls

--- module2/train.py
+++
@@ -1,7 +0,0 @@
-a = input(">>> ")
-print(type(a))
-
-if type(a) == float:
-    print('corrrect')
-else:
-    print('try one more time')
--- module6/4_14.py
+++
@@ -1,8 +0,0 @@
-def add_employee_to_file(record, path):
-    file = open(path, "a")
-    file.write(f"\n{record}")
-    file.close()
-
-
-add_employee_to_file("Ya suka i mne 14",
-                     "/Users/black/Documents/GoIt_Lessons/module6/train/rabotniki.txt")
